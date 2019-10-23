3702aaad71609cd821bab9338d1eed58d93bdbea
Gaillard <38909132+WaRobby78@users.noreply.github.com>
2019-10-23 10:25:15 +0200

    Create Ressources.java
    test

---
+++ Ressources.java
@@ -0,0 +1,26 @@
+public class Client
+{
+    private String nom;
+    private String contrat;
+
+    public Client(String nom, String contrat){
+        this.nom = nom;
+        this.contrat = contrat;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getContrat() {
+        return contrat;
+    }
+
+    public void setContrat(String contrat) {
+        this.contrat = contrat;
+    }
+}
