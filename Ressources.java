ff1a5cebb244d9fa93ac28ff2306e59380d297d8
rogailla <rogailla@LFR038900.corp.capgemini.com>
2019-10-29 11:48:12 +0100

    some tests

--- Ressources.java
+++ Ressources.java
@@ -2,6 +2,43 @@ import java.util.Date;
 import java.sql.*;
 
 public class Ressources {
+	
+	public static void show_Tables(Connection connection)
+    {
+		//for more informations !!!!
+		//https://stackoverflow.com/questions/34774331/how-to-get-table-information-in-a-database-sqlite
+		
+		
+		try
+		{
+			ResultSet rs = connection.getMetaData().getTables(null, null, null, null);
+		    while (rs.next())
+		    {
+		        System.out.println(rs.getString("TABLE_NAME"));
+		    }
+		}
+	    catch(SQLException e)
+	    {
+	    	// if the error message is "out of memory", 
+	    	// it probably means no database file is found
+	    	System.err.println(e.getMessage());
+	    }
+	    finally
+	    {
+	    	try
+	    	{
+	    		if(connection != null)
+	    			connection.close();
+	    	}
+	    	catch(SQLException e)
+	    	{
+	    		// connection close failed.
+	    		System.err.println(e);
+	    	}
+	    }
+	    
+        return ;
+    }
 
 	public static void main(String[] args) throws ClassNotFoundException
 	{
@@ -25,16 +62,26 @@ public class Ressources {
 	    	
 	    	statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-	    	statement.executeUpdate("drop table if exists person");
-	    	statement.executeUpdate("create table Salle (id integer, name string)");
-	    	statement.executeUpdate("insert into person values(1, 'leo')");
-	    	ResultSet rs = statement.executeQuery("select * from person");
+	    	statement.executeUpdate("drop table if exists Salle");
+	    	statement.executeUpdate("drop table if exists Personne");
+	    	//statement.executeUpdate("drop table if exists Creneau");
+	    	
+	    	statement.executeUpdate("create table Salle (IDSalle integer, Name string, constraint IDSPK PRIMARY KEY (IDSalle))");
+	    	statement.executeUpdate("create table Personne (IDPersonne integer, name string, constraint IDPPK PRIMARY KEY (IDPersonne))");
+	    	//statement.executeUpdate("create table Creneau (start date, end date)");
+	    	
+	    	show_Tables(connection);
+	    	
+	    	/*statement.executeUpdate("insert into person values(1, 'leo')");
+	    	ResultSet rs = statement.executeQuery("SELECT * FROM sqlite_master");
 	    	while(rs.next())
 	    	{
 	    		// read the result set
 	    		System.out.println("name = " + rs.getString("name"));
 	    		System.out.println("id = " + rs.getInt("id"));
-	    	}
+	    		System.out.println(rs);
+	    	}*/
+	    	
 	    }
 	    catch(SQLException e)
 	    {
@@ -55,6 +102,8 @@ public class Ressources {
 	    		System.err.println(e);
 	    	}
 	    }
+	    
+	    System.out.println("pas de bug");
 		
 		return;
 	}
